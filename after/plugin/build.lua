d71e83d0fe18cc0142832c059f3fc5a939ec2dee
Cameron Bannasch <cameron.bannasch@gmail.com>
2024-01-19 22:59:46 -0800

    Add deploy keybind

--- after/plugin/build.lua
+++ after/plugin/build.lua
@@ -5,21 +5,31 @@ end
 
 
 function Run_gradle_deploy()
-    local job_id = vim.fn.jobstart('./gradlew build', {
-        on_stderr = function(_, data, _)
-            print(data)
-        end,
-        on_stdout = function(_, data, _)
-            print(data)
-        end,
+    local output = ""
+    local notification
+    local notify = function(msg, level)
+        local notify_opts = vim.tbl_extend(
+        "keep",
+        opts or {},
+        { replace = notification }
+        )
+        notification = vim.notify(msg, level, notify_opts)
+    end
+    notify("Deploying code to roborio")
+    local on_data = function(_, data)
+        output = output .. table.concat(data, "\n")
+    end
+    local job_id = vim.fn.jobstart('./gradlew deploy --console plain', {
+        on_stderr = on_data,
+        on_stdout = on_data,
+        on_data = on_data,
         on_exit = function(_, code, _)
             if code == 0 then
-                print("Gradle command executed successfully.")
+                notify("Gradle deploy executed successfully.", "info")
             else
-                print("Gradle command failed with code " .. code .. ".")
+                notify("Gradle deploy failed with code " .. code .. ".\n" .. output, "error")
             end
         end,
-        detach = true,
         cwd = find_git_root(),
     })
 end
